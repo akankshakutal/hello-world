d54afac8d18d392bc658c81082532554989e1012
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-04-16 17:42:55 +0530

    update gitignore

---
+++ build/precache-manifest.2894412cd433f52fef1350f5230c563e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12957d6ec492c604e9e5",
+    "url": "/static/css/main.1229b758.chunk.css"
+  },
+  {
+    "revision": "12957d6ec492c604e9e5",
+    "url": "/static/js/main.6f029cc4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "d57cf400959860484a66d7f8982ed969",
+    "url": "/index.html"
+  }
+];
